9fce96fb73b75fd1e36af4e2c9d374b90d627440
Ankan Sharma <ankanroni3@gmail.com>
2021-06-25 23:44:49 +0530

    tokenized sentence and reversed input sentences

--- data_utils/data_preparation.py
+++ data_utils/data_preparation.py
@@ -14,7 +14,16 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-
 spacy_fr = spacy.load('fr_core_news_sm')
 spacy_en = spacy.load('en_core_web_sm')
+
+
+def tokenize_fr(text):
+    # Reversing sentences to get long term benefit
+    return [tok.text for tok in spacy_fr.tokenizer(text)][::-1]
+
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
 
