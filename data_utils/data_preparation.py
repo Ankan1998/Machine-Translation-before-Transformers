460a6604f15d0505e13b6fa16be7a48fd578662c
Ankan Sharma <ankanroni3@gmail.com>
2021-06-25 23:54:13 +0530

    loaded Data

--- data_utils/data_preparation.py
+++ data_utils/data_preparation.py
@@ -14,16 +14,32 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-spacy_fr = spacy.load('fr_core_news_sm')
+spacy_de = spacy.load('de_core_news_sm')
 spacy_en = spacy.load('en_core_web_sm')
 
 
-def tokenize_fr(text):
+def tokenize_de(text):
     # Reversing sentences to get long term benefit
-    return [tok.text for tok in spacy_fr.tokenizer(text)][::-1]
+    return [tok.text for tok in spacy_de.tokenizer(text)][::-1]
 
 
 def tokenize_en(text):
     return [tok.text for tok in spacy_en.tokenizer(text)]
 
 
+src = Field(tokenize=tokenize_de,
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+trg = Field(tokenize=tokenize_en,
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+
+train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'),
+                                                    fields=(src, trg))
+print(f"Number of training examples: {len(train_data.examples)}")
+print(f"Number of validation examples: {len(valid_data.examples)}")
+print(f"Number of testing examples: {len(test_data.examples)}")
