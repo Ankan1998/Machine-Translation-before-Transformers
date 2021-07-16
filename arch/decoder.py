c804c8e5ea891e5a84b8783ac81d27b261ab3818
Ankan Sharma <ankanroni3@gmail.com>
2021-07-16 13:37:00 +0530

    Decoder with (unidirection obvio) num layer = 1

--- arch/decoder.py
+++ arch/decoder.py
@@ -1,15 +1,19 @@
 import torch
 import torch.nn as nn
-from data_utils.data_preparation import vocab_builder, data_loader
+from data_utils.data_preparation import DataCreator
 
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers=2, dropout=0.2):
+    def __init__(self, output_dim, emb_dim, hid_dim, dropout=0.2):
         super().__init__()
 
+        self.output_dim = output_dim
+        self.hid_dim = hid_dim
+        #self.n_layers = n_layers
+
         self.embedding = nn.Embedding(output_dim, emb_dim)
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, bidirectional=True, dropout=dropout)
-        self.fc_out = nn.Linear(2 * hid_dim, output_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, dropout=dropout)
+        self.fc_out = nn.Linear(hid_dim, output_dim)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, input, hidden, cell):
@@ -22,14 +26,16 @@ class Decoder(nn.Module):
 
 
 if __name__ == "__main__":
-    train_data, val_data, test_data = data_loader()
-    source, target = vocab_builder(train_data)
-    target_size_encoder = len(target.vocab)
-    decoder_embedding_size = 300
-    hidden_size = 1024
-    num_layers = 2
-    dropout = float(0.5)
-
-    decoder_lstm = Decoder(target_size_encoder, decoder_embedding_size,
-                           hidden_size, num_layers, dropout)
-    print(decoder_lstm)
+    pass
+    # data_c = DataCreator()
+    # train_data, val_data, test_data = data_c.data_loader('data_utils/.data')
+    # source, target = data_c.vocab_builder(train_data)
+    # target_size_encoder = len(target.vocab)
+    # decoder_embedding_size = 300
+    # hidden_size = 1024
+    # num_layers = 2
+    # dropout = float(0.5)
+    #
+    # decoder_lstm = Decoder(target_size_encoder, decoder_embedding_size,
+    #                        hidden_size, num_layers, dropout)
+    # print(decoder_lstm)
